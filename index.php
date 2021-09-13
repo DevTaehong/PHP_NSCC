0163f253d80a7497f5a9d04ec881da56aaacdad7
Taehong <minth1123@icloud.com>
2021-09-13 14:29:20 -0300

    Added first file and discovered the meaning of life ..... not really

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <h1>
+            <?php
+                echo "Hello from PHPStorm!";
+                ?>
+        </h1>
+        <p>
+            <?php
+                $number1 = 10;
+                $number2 = 32;
+                echo "the meaning of life is" . ($number1 + $number2);
+                ?>
+        </p>
+    </body>
+</html>
