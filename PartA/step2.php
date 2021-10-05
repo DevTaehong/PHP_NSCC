7453e8f5db1bc46ae94537d833e69300e085f153
Taehong <minth1123@icloud.com>
2021-10-05 01:19:50 -0300

    Lab 3 done

---
+++ PartA/step2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Our film List</title>
+        <style>
+            table, th, tr, td { border: solid 2px red;}
+        </style>
+    </head>
+    <body>
+        <table border="1">
+            <thead>
+                <th>Film</th>
+                <th>Description</th>
+            </thead>
+            <tbody>
+            <?php
+                function getDbConnection()
+                {
+                    $conn = mysqli_connect("database", "root", "inet2005", "sakila");
+                    if(!$conn)
+                    {
+                        die("Unable to connect to database: " . mysqli_connect_error());
+                    }
+
+                    return $conn;
+                }
+            ?>
+            <?php
+                $conn = getDbConnection();
+                $des = $_POST['search'];
+
+                $result = mysqli_query($conn,"SELECT * FROM film WHERE description LIKE '%$des%'");
+                if(!$result)
+                {
+                    die("Could not retrieve records from database: " . mysqli_error($conn));
+                }
+
+                while($row = mysqli_fetch_assoc($result)):
+            ?>
+                <tr>
+                    <td><?php echo $row['title'] ?></td>
+                    <td><?php echo $row['description'] ?></td>
+                </tr>
+            <?php
+                endwhile;
+                mysqli_close($conn);
+            ?>
+            </tbody>
+        </table>
+    </body>
+</html>
---
+++ PartC/index.js
@@ -0,0 +1,108 @@
+function italicsText(fieldID)
+{
+    var myFormItem = document.getElementById(fieldID);
+    if(myFormItem != null)
+    {
+        myFormItem.style.fontStyle = "italic";
+    }
+}
+
+function normalText(fieldID)
+{
+    var myFormItem = document.getElementById(fieldID);
+    if(myFormItem != null)
+    {
+        myFormItem.style.fontStyle = "normal";
+    }
+}
+
+function BGColorText(fieldID)
+{
+    var myFormItem = document.getElementById(fieldID);
+    if(myFormItem != null)
+    {
+        myFormItem.style.backgroundColor = "yellow";
+    }
+}
+
+function BGColorTextBack(fieldID)
+{
+    var myFormItem = document.getElementById(fieldID);
+    if(myFormItem != null)
+    {
+        myFormItem.style.backgroundColor = "";
+    }
+}
+
+function UnderlineText(num)
+{
+    switch (num){
+        case 1: document.getElementById("labelFName").style.textDecoration = "underline";
+            break;
+        case 2: document.getElementById("labelLName").style.textDecoration = "underline";
+            break;
+        case 3:document.getElementById("labelAddress1").style.textDecoration = "underline";
+            break;
+        case 4:document.getElementById("labelAddress2").style.textDecoration = "underline";
+            break;
+        case 5:document.getElementById("labelEmail").style.textDecoration = "underline";
+            break;
+    }
+}
+
+function NonUnderlineText(num)
+{
+    switch (num){
+        case 1: document.getElementById("labelFName").style.textDecoration = "none";
+            break;
+        case 2: document.getElementById("labelLName").style.textDecoration = "none";
+            break;
+        case 3:document.getElementById("labelAddress1").style.textDecoration = "none";
+            break;
+        case 4:document.getElementById("labelAddress2").style.textDecoration = "none";
+            break;
+        case 5:document.getElementById("labelEmail").style.textDecoration = "none";
+            break;
+    }
+}
+
+function validateForm() {
+    let valid = 0;
+
+    if (document.forms["myForm"].fName.value.length == 0) {
+        document.getElementById("fName").style.borderColor = "red";
+        valid = 1;
+    }
+    if (document.forms["myForm"].lName.value.length == 0) {
+        document.getElementById("lName").style.borderColor = "red";
+         valid = 1;
+    }
+    if (document.forms["myForm"].address1.value.length == 0) {
+        document.getElementById("address1").style.borderColor = "red";
+         valid = 1;
+    }
+    if (document.forms["myForm"].address2.value.length == 0) {
+        document.getElementById("address2").style.borderColor = "red";
+         valid = 1;
+    }
+    if (document.forms["myForm"].email.value.length == 0) {
+        document.getElementById("email").style.borderColor = "red";
+         valid = 1;
+    }
+
+    if (document.forms["myForm"].box.checked == false){
+        document.getElementById("boxLabel").style.display = "inline-block";
+        document.getElementById("boxLabel").style.color="red";
+        valid = 1;
+    }
+
+    if (valid == 1) {
+        return false;
+    }
+
+    if (valid != 1) {
+        return true;
+    }
+
+
+}
