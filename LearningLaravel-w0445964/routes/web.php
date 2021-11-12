504a7ab06c97f0b56c09a898342b4135576518a6
Taehong <minth1123@icloud.com>
2021-11-11 23:08:42 -0400

    Video 10 complete

--- LearningLaravel-w0445964/routes/web.php
+++ LearningLaravel-w0445964/routes/web.php
@@ -26,6 +26,9 @@ Route::post('/task', 'TaskController@store');
 Route::get('/customers', 'CustomerController@index');
 Route::get('/customers/create', 'CustomerController@create');
 Route::post('/customers', 'CustomerController@store');
+Route::get('/customers/{customer}', 'CustomerController@show');
+Route::get('/customers/{customer}/edit', 'CustomerController@edit');
+Route::patch('/customers/{customer}/', 'CustomerController@update');
 
 
 
